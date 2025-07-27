796df1fd910b33e2e7fb47908d27cb58306f89d7
Parviz <p.ahmadov@student.tudelft.nl>
2025-07-28 00:01:42 +0200

    docs added

--- recbole/trainer/trainer.py
+++ recbole/trainer/trainer.py
@@ -145,7 +145,7 @@ class Trainer(AbstractTrainer):
         self.learning_rate = config["learning_rate"]
         self.epochs = 1000
         self.eval_step = min(config["eval_step"], self.epochs)
-        self.stopping_step = 30
+        self.stopping_step = 70
         self.clip_grad_norm = config["clip_grad_norm"]
         self.valid_metric = config["valid_metric"].lower()
         self.valid_metric_bigger = config["valid_metric_bigger"]
