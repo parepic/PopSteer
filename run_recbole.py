2a4101a68195bdd260f1f07f9510cf6521396f75
Parviz <p.ahmadov@student.tudelft.nl>
2025-07-27 23:01:34 +0200

    docs added

--- run_recbole.py
+++ run_recbole.py
@@ -106,7 +106,7 @@ if __name__ == "__main__":
                 # "load": "./saved/sasrec_yoochoose-32-48.pth",
                 "sae_scale_size": [32, 96],
                 "sae_k": [48, 64],
-                "learning_rate": 1e-4,
+                "learning_rate": 1e-3,
                 "alpha": [1.0, 1.0],
                 "steer": [0, 0],
                 "analyze": False,
