fc445d05a34e5b4c0dd46e83277bd8bd675f5271
Parviz <p.ahmadov@student.tudelft.nl>
2025-07-28 20:36:07 +0200

    metric addition

--- run_recbole.py
+++ run_recbole.py
@@ -116,11 +116,12 @@ if __name__ == "__main__":
                 "metrics": ["Recall","NDCG","Hit", "Deep_LT_Coverage", "GiniIndex", "AveragePopularity", "ItemCoverage", "NDCGTail", "NDCGHead", "NDCGMid", "NDCGUserTail", "NDCGUserMid", "NDCGUserHead"],
                 "train_neg_sample_args": None,
                 "hidden_size": 128,
-                "input_dim": 128,
-                "valid_metric": "SAE_LOSS_u"
+                "input_dim": 128
                 }
         if args.model in ["LightGCN_SAE", "SASRec_SAE"]:
             config_dict["metrics"].extend(["SAE_Loss_i", "SAE_Loss_u", "SAE_Loss_total"])
+            config_dict["valid_metric"] = "SAE_LOSS_u"
+
         run(
             args.model,
             args.dataset,
