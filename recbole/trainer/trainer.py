197fa2e0c5187a72ea6bb26fe764c9bb1ebbc3dc
Parviz <p.ahmadov@student.tudelft.nl>
2025-08-01 22:08:23 +0200

    book test

--- recbole/trainer/trainer.py
+++ recbole/trainer/trainer.py
@@ -750,7 +750,7 @@ class Trainer(AbstractTrainer):
             if show_progress
             else data
         )
-        times = 200
+        times = 250
         cur = 0
         for batch_idx, batched_data in enumerate(iter_data):
             if cur >= times:
