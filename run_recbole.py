b4d5b0e39a1258c941fdb86f1fa5ff56e7a82bc7
parviz <parvizahmedov2002@gmail.com>
2025-08-06 01:45:04 +0200

    ml 1m test

--- run_recbole.py
+++ run_recbole.py
@@ -121,7 +121,7 @@ if __name__ == "__main__":
                 "load": "./saved/sasrec_ml-1m-32-32.pth",
                 "sae_scale_size": [64, 64],
                 "sae_k": [32, 32],
-                "learning_rate": 1e-4,
+                "learning_rate": 1e-5,
                 "alpha": [1.0, 1.0],
                 "steer": [0, 0],
                 "steer_dir": [0, 0],
