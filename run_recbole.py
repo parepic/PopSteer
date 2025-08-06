e4addb49a1c716c478d1d3ebe4460938eb7ffeba
parviz <parvizahmedov2002@gmail.com>
2025-08-07 00:11:41 +0200

    beeradvocate train

--- run_recbole.py
+++ run_recbole.py
@@ -122,7 +122,7 @@ if __name__ == "__main__":
         if args.config_json is None:
             config_dict = {
                 "base_path": "./saved/sasrec_ml-1m.pth",
-                # "load": "./saved/sasrec_ml-1m-32-48.pth",
+                "load": "./saved/sasrec_beer.pth",
                 "sae_scale_size": [64, 64],
                 "sae_k": [32, 40],
                 "learning_rate": 1e-3,
