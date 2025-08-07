a8d8fa4de282d98c74e10b7e78d5c99b44c7b335
parviz <parvizahmedov2002@gmail.com>
2025-08-07 20:01:29 +0200

    beer test

--- run_recbole.py
+++ run_recbole.py
@@ -131,7 +131,7 @@ if __name__ == "__main__":
     if args.train == True:
         if args.config_json is None:
             config_dict = {
-                "base_path": "./saved/sasrec_ml-1m.pth",
+                "base_path": "./saved/sasrec_beer.pth",
                 # "load": "./saved/sasrec_ml-1m-32-40.pth",
                 "sae_scale_size": [64, 64],
                 "sae_k": [32, 48],
