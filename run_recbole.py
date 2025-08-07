a7ece0efe7e201ae9e880c9f9456a015887c001f
parviz <parvizahmedov2002@gmail.com>
2025-08-07 20:00:08 +0200

    beer test

--- run_recbole.py
+++ run_recbole.py
@@ -133,9 +133,9 @@ if __name__ == "__main__":
             config_dict = {
                 "base_path": "./saved/sasrec_ml-1m.pth",
                 # "load": "./saved/sasrec_ml-1m-32-40.pth",
-                "sae_scale_size": [64, 128],
-                "sae_k": [32, 44],
-                "learning_rate": 1e-3,
+                "sae_scale_size": [64, 64],
+                "sae_k": [32, 48],
+                "learning_rate": 1e-4,
                 "alpha": [1.0, 1.0],
                 "steer": [0, 0],
                 "steer_dir": [0, 0],
